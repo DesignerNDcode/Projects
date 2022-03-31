3967a87fafe31f33b75084db8d73c728075ec03f
DesignerNDcode <87579869+DesignerNDcode@users.noreply.github.com>
2022-03-31 22:18:09 +0500

    Add files via upload

---
+++ Task#4.py
@@ -0,0 +1,8 @@
+import random
+Mates = ["Muhaimin", "Shoiab", "Yasir", "Nazzal", "Rehan", "Hammad"]
+i = 1
+score = []
+while i <= 6:
+    i = i+1
+    res = random.randrange(60, 90, 1)
+    score.append(res)
